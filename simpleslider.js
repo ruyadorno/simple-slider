13280604ae7706843dcb783d79296de3bb19ba28
Ruy Adorno <ruyadorno@gmail.com>
2013-05-29 13:41:07 -0300

    Reduced some var names

--- simpleslider.js
+++ simpleslider.js
@@ -1,15 +1,18 @@
 (function(){
 	'use strict';
+	function getdef(val, def){
+		return val ? val : def;
+	}
 	var SimpleSlider = function(containerElem, options){
 		this.containerElem = containerElem;
 		if( !options ) options = {};
-		this.transitionProperty = options.transitionProperty ? options.transitionProperty : 'opacity';
-		this.transitionValue =  options.transitionValue ? options.transitionValue : 0;
-		this.transitionTime = options.transitionTime ? options.transitionTime : 0.5;
-		this.transitionDelay = options.transitionDelay ? options.transitionDelay : 1;
-		this.startValue = options.startValue ? options.startValue : 100;
-		this.endValue = options.endValue ? options.endValue : 0;
-		this.autoPlay = options.autoPlay ? options.autoPlay : true;
+		this.trProp = getdef(options.transitionProperty);
+		this.trVal =  getdef(options.transitionValue);
+		this.trTime = getdef(options.transitionTime);
+		this.delay = getdef(options.transitionDelay);
+		this.startVal = getdef(options.startValue);
+		this.endVal = getdef(options.endValue);
+		this.autoPlay = getdef(options.autoPlay);
 		this.init();
 	};
 	SimpleSlider.prototype.init = function(){
@@ -18,50 +21,50 @@
 		this.imgs = [];
 		while(i>=0){
 			this.imgs[i] = bannerElem = this.containerElem.children[i];
-			bannerElem.style[this.transitionProperty] = this.endValue;
+			bannerElem.style[this.trProp] = this.endVal;
 			i--;
 		}
-		this.imgs[0].style[this.transitionProperty] = this.startValue;
+		this.imgs[0].style[this.trProp] = this.startVal;
 		this.actualIndex = 0;
 		if( this.autoPlay ){
 			var scope = this;
 			setInterval(function(){
-				scope.changeSimpleSlider(scope.nextSimpleSliderIndex());
-			}, this.transitionDelay*1000);
+				scope.change(scope.nextIndex());
+			}, this.delay*1000);
 		}
 	};
 	SimpleSlider.prototype.anim = function(target, diffValue, targetValue){
-		var nextValue = this.transitionValue+(diffValue/60);
-		this.transitionValue = nextValue;
-		target.style[this.transitionProperty] = this.transitionValue/100;
+		var nextValue = this.trVal+(diffValue/60);
+		this.trVal = nextValue;
+		target.style[this.trProp] = this.trVal/100;
 		var isPositive = diffValue>0 && nextValue<targetValue;
 		var isNegative = diffValue<=0 && nextValue>targetValue;
 		if( isPositive || isNegative ){
 			var scope = this;
 			window.setTimeout(function(){
 				scope.anim.apply(scope, [target, diffValue, targetValue]);
-			}, (1000*scope.transitionTime)/60);
+			}, ( 1000 * scope.trTime ) / 60 );
 		} else {
-			target.style[this.transitionProperty] = diffValue;
+			target.style[this.trProp] = diffValue;
 		}
 	};
 	SimpleSlider.prototype.startAnim = function(target, fromValue, targetValue){
-		this.transitionValue = fromValue;
-		var animEndValue = targetValue-this.transitionValue;
+		this.trVal = fromValue;
+		var animEndValue = targetValue-this.trVal;
 		this.anim(target, animEndValue, targetValue);
 	};
-	SimpleSlider.prototype.removeSimpleSlider = function(index){
-		this.startAnim(this.imgs[index], this.startValue, this.endValue);
+	SimpleSlider.prototype.remove = function(index){
+		this.startAnim(this.imgs[index], this.startVal, this.endVal);
 	};
-	SimpleSlider.prototype.insertSimpleSlider = function(index){
-		this.startAnim(this.imgs[index], this.endValue, this.startValue);
+	SimpleSlider.prototype.insert = function(index){
+		this.startAnim(this.imgs[index], this.endVal, this.startVal);
 	};
-	SimpleSlider.prototype.changeSimpleSlider = function(newIndex){
-		this.removeSimpleSlider(this.actualIndex);
-		this.insertSimpleSlider(newIndex);
+	SimpleSlider.prototype.change = function(newIndex){
+		this.remove(this.actualIndex);
+		this.insert(newIndex);
 		this.actualIndex = newIndex;
 	};
-	SimpleSlider.prototype.nextSimpleSliderIndex = function(){
+	SimpleSlider.prototype.nextIndex = function(){
 		var newIndex = this.actualIndex+1;
 		if( newIndex >= this.imgs.length ){
 			newIndex = 0;
