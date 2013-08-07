307a5ce23c1d4509bbcea9e42ccf7959d21c3078
Ruy Adorno <ruyadorno@hotmail.com>
2013-08-07 18:54:56 -0300

    Added support to amd/commonjs

--- simpleslider.js
+++ simpleslider.js
@@ -1,8 +1,21 @@
-(function(){
+(function (context, definition) {
+
+	if (typeof module != 'undefined' && module.exports) {
+		module.exports = definition();
+	} else if (typeof define == 'function' && define.amd) {
+		define(definition);
+	} else {
+		window.SimpleSlider = definition();
+	}
+
+})(this, function () {
+
 	'use strict';
+
 	function getdef(val, def){
 		return val===undefined || val===null ? def : val;
 	}
+
 	var SimpleSlider = function(containerElem, options){
 		this.containerElem = containerElem;
 		if( !options ) options = {};
@@ -15,6 +28,7 @@
 		this.autoPlay = getdef(options.autoPlay, true);
 		this.init();
 	};
+
 	SimpleSlider.prototype.init = function(){
 		var i = this.containerElem.children.length-1;
 		var bannerElem = null;
@@ -33,6 +47,7 @@
 			}, this.delay*1000);
 		}
 	};
+
 	SimpleSlider.prototype.anim = function(target, diffValue, targetValue){
 		var nextValue = this.trVal+(diffValue/60);
 		this.trVal = nextValue;
@@ -48,22 +63,27 @@
 			target.style[this.trProp] = diffValue;
 		}
 	};
+
 	SimpleSlider.prototype.startAnim = function(target, fromValue, targetValue){
 		this.trVal = fromValue;
 		var animEndValue = targetValue-this.trVal;
 		this.anim(target, animEndValue, targetValue);
 	};
+
 	SimpleSlider.prototype.remove = function(index){
 		this.startAnim(this.imgs[index], this.startVal, this.endVal);
 	};
+
 	SimpleSlider.prototype.insert = function(index){
 		this.startAnim(this.imgs[index], this.endVal, this.startVal);
 	};
+
 	SimpleSlider.prototype.change = function(newIndex){
 		this.remove(this.actualIndex);
 		this.insert(newIndex);
 		this.actualIndex = newIndex;
 	};
+
 	SimpleSlider.prototype.nextIndex = function(){
 		var newIndex = this.actualIndex+1;
 		if( newIndex >= this.imgs.length ){
@@ -71,5 +91,7 @@
 		}
 		return newIndex;
 	};
-	window.SimpleSlider = SimpleSlider;
-})();
+
+	return SimpleSlider;
+
+});
