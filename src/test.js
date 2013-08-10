66ebfe8e568071fa2574787049f7a894c986fdc4
Ruy Adorno <ruyadorno@hotmail.com>
2013-08-10 15:08:03 -0300

    Adding test case for properties initialization

--- src/test.js
+++ src/test.js
@@ -1,10 +1,56 @@
 describe('SimpleSlider', function() {
 
-    var testDiv;
+    var testDivCount = 0;
 
-    it('should create a new instance', function(){
-        testDiv = document.getElementById('test-div');
-        expect(typeof new SimpleSlider(testDiv)).toEqual('object');
+    var getNewDiv = function() {
+
+        var newDiv = document.createElement('div');
+        newDiv.id = 'test-div-' + testDivCount;
+        testDivCount++;
+
+        addChildrenDivs(newDiv);
+
+        return newDiv;
+
+    };
+
+    var addChildrenDivs = function(newDiv) {
+
+        var childrenNum = Math.ceil( Math.random()*10 );
+        while (childrenNum >= 0) {
+            newDiv.appendChild(document.createElement('div'));
+            childrenNum--;
+        }
+
+    };
+
+    var getNewSlider = function() {
+
+        var testDiv = getNewDiv();
+        var ss = new SimpleSlider(testDiv);
+
+        return ss;
+
+    };
+
+    it('should create a new instance', function() {
+
+        expect(typeof getNewSlider()).toEqual('object');
+
+    });
+
+    it('properties should be defined properly', function(){
+
+        // Test default values
+        var ss = getNewSlider();
+        expect(ss.trProp).toEqual('opacity');
+        expect(ss.trVal).toEqual(0);
+        expect(ss.trTime).toEqual(0.5);
+        expect(ss.delay).toEqual(1);
+        expect(ss.startVal).toEqual(100);
+        expect(ss.endVal).toEqual(0);
+        expect(ss.autoPlay).toEqual(true);
+
     });
 
 });
