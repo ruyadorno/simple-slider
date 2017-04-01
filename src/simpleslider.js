374e448e3af5e90390296c5ba09e4994a994e7ea
Ruy Adorno <ruyadorno@hotmail.com>
2017-04-01 17:26:01 -0400

    Fixed z-index issue

--- src/simpleslider.js
+++ src/simpleslider.js
@@ -121,8 +121,13 @@ function getSlider(options) {
   }
 
   function change(newIndex) {
-    imgs[newIndex].style.zIndex = 2;
-    imgs[actualIndex].style.zIndex = 1;
+    let count = imgs.length;
+    while (--count >= 0) {
+      imgs[count].style.zIndex = 1;
+    }
+
+    imgs[newIndex].style.zIndex = 3;
+    imgs[actualIndex].style.zIndex = 2;
 
     anim([
       {
